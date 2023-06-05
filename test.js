8273df70e303e81f90451deac091c456a009c0ea
XhmikosR <xhmikosr@gmail.com>
2023-06-05 14:40:44 +0300

    Update file-type to v18 and downgrade ava to v4

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 import isJpg from 'is-jpg';
 import test from 'ava';
-import m from './index.js';
+import decompressTarGz from './index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 test('extract file', async t => {
 	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'file.tar.gz'));
-	const files = await m()(buf);
+	const files = await decompressTarGz()(buf);
 
 	t.is(files[0].path, 'test.jpg');
 	t.true(isJpg(files[0].data));
@@ -18,7 +18,7 @@ test('extract file', async t => {
 
 test('extract file using streams', async t => {
 	const stream = fs.createReadStream(path.join(__dirname, 'fixtures', 'file.tar.gz'));
-	const files = await m()(stream);
+	const files = await decompressTarGz()(stream);
 
 	t.is(files[0].path, 'test.jpg');
 	t.true(isJpg(files[0].data));
@@ -26,17 +26,17 @@ test('extract file using streams', async t => {
 
 test('return empty array if non-valid file is supplied', async t => {
 	const buf = await fsP.readFile(__filename);
-	const files = await m()(buf);
+	const files = await decompressTarGz()(buf);
 
 	t.is(files.length, 0);
 });
 
 test('throw on wrong input', async t => {
-	await t.throwsAsync(m()('foo'), undefined, 'Expected a Buffer or Stream, got string');
+	await t.throwsAsync(decompressTarGz()('foo'), undefined, 'Expected a Buffer or Stream, got string');
 });
 
 test('handle gzip error', async t => {
 	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'fail.tar.gz'));
-	const error = await t.throwsAsync(m()(buf), undefined, 'unexpected end of file');
+	const error = await t.throwsAsync(decompressTarGz()(buf), undefined, 'unexpected end of file');
 	t.is(error.code, 'Z_BUF_ERROR');
 });
